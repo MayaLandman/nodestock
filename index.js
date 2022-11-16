12d8958e248240108a1103777414e1cc676f999f
mayaLandman <maya.landman@gmail.com>
2022-11-17 00:26:28 +0200

    Created procfile for heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const exphbs = require('express-handlebars');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,13 +10,18 @@ const PORT = process.env.PORT || 5000;
 app.engine('handlebars', require('exphbs'));
 app.set('view engine', 'handlebars');
 
-
+request('htps://cloud.iexapis.com/stable/stock/fb/quote?token=pk_0ab9c13fc6374b5e859fe290b19e2324', {json: true}, (err,res,nody) =>{
+    if(err){return console.log(err);}
+    console.log(body); 
+    if(res.statusCode === 200){
+        console.log(body);
+    };
+});
+    
 
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-
 
 
 
